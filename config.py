6a34e23d8424bb65abf23062c2104a96541421fd
ghadgeharshu01@gmail.com <ghadgeharshu01@gmail.com>
2024-12-07 17:55:15 +0530

    file

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+model_path = os.path.join("Project","KNN_MODEL.pkl")  ##Project\linear_model.pkl
+json_path = os.path.join("Project","label_encode.json")
+std_scale_path = os.path.join("Project","scale.pkl")
